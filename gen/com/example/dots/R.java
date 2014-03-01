07d2685cbe9534337e104c15c8e7bcde748b094a
vish1993 <visaahan@hotmail.com>
2014-03-01 15:02:41 -0500

    Got fading and randomly moving around working

--- gen/com/example/dots/R.java
+++ gen/com/example/dots/R.java
@@ -10,9 +10,6 @@ package com.example.dots;
 public final class R {
     public static final class attr {
     }
-    public static final class color {
-        public static final int black=0x7f060000;
-    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -29,21 +26,21 @@ public final class R {
         public static final int red_circle=0x7f020002;
     }
     public static final class id {
-        public static final int ImageButton01=0x7f090006;
-        public static final int ImageButton02=0x7f090002;
-        public static final int ImageButton03=0x7f090004;
-        public static final int ImageButton04=0x7f090007;
-        public static final int action_settings=0x7f090008;
-        public static final int play_text=0x7f090005;
-        public static final int rect=0x7f090000;
-        public static final int relativeLayout1=0x7f090003;
-        public static final int textView1=0x7f090001;
+        public static final int ImageButton01=0x7f080006;
+        public static final int ImageButton02=0x7f080002;
+        public static final int ImageButton03=0x7f080004;
+        public static final int ImageButton04=0x7f080007;
+        public static final int action_settings=0x7f080008;
+        public static final int play_text=0x7f080005;
+        public static final int rect=0x7f080000;
+        public static final int relativeLayout1=0x7f080003;
+        public static final int textView1=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int main=0x7f070000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -74,10 +71,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f060001;
     }
 }
--- src/com/example/dots/MainActivity.java
+++ src/com/example/dots/MainActivity.java
@@ -1,37 +1,92 @@
-package com.example.dots;
+package com .example.dots;
 
+import java.util.Random;
+
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.Menu;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
+@SuppressLint("NewApi")
 public class MainActivity extends Activity {
-
-	@Override
-
-    protected void onCreate(Bundle savedInstanceState) {
+	
+    @SuppressWarnings("deprecation")
+	protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        ImageView iv = new ImageView (this);
+
+        //To figure setting out how to create image views through code
+        final ImageView iv = new ImageView (this);
         iv.setImageResource(R.drawable.blue_circle);
+        iv.setVisibility (View.INVISIBLE);
+        
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        final int width = size.x - 110;
+        final int height = size.y - 110;
+        
+        //Layout params to place image view on
         RelativeLayout r1 = (RelativeLayout)findViewById (R.id.rect);
         RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams (
         		RelativeLayout.LayoutParams.WRAP_CONTENT,
         		RelativeLayout.LayoutParams.WRAP_CONTENT
-        		);
+        );
+        
+        //Set up height and width of layout
+        lp.height = 110;
+        lp.width = 110;
         lp.addRule (RelativeLayout.BELOW, R.id.ImageButton02);
         lp.addRule (RelativeLayout.ALIGN_PARENT_RIGHT);
         r1.addView (iv,lp);
+        
+        final Random x = new Random();
+        final Random y = new Random();
+        //defining the animations
+        Animation fadeIn = new AlphaAnimation(0f, 1.0f);
+        fadeIn.setInterpolator(new LinearInterpolator()); //add this
+        fadeIn.setDuration(3000);
+
+        Animation fadeOut = new AlphaAnimation(1.0f, 0f);
+        fadeOut.setInterpolator(new LinearInterpolator()); //and this
+        fadeOut.setStartOffset(3000);
+        fadeOut.setDuration(3000);
+
+        AnimationSet animation = new AnimationSet(false); //change to false
+        animation.addAnimation(fadeIn);
+        animation.addAnimation(fadeOut);
+    
+        animation.setAnimationListener (new Animation.AnimationListener() {
+    		public void onAnimationStart (Animation animation){
+    		}
+    		public void onAnimationEnd (Animation animation){	
+    			float next_x = (x.nextInt(width)) * -1;//need to make it so can fit within screen
+    			float next_y = (y.nextInt(height));//
+    			iv.setTranslationX (next_x);
+    			iv.setTranslationY(next_y);
+    			animation.reset();
+    			animation.start();
+    		}
+    		public void onAnimationRepeat (Animation animation) {}
+    	});
+    	iv.startAnimation (animation);
     }
 
 
-    @Override
+    @SuppressLint("NewApi")
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
 }
